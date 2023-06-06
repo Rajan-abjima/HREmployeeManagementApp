e6a2b30eb9a5ecbf2e246dd0dd6ade67ebf20874
Rajan-abjima <125352988+Rajan-abjima@users.noreply.github.com>
2023-06-06 19:08:53 +0530

    06-06-23

---
+++ HREmployeeManagementApp/Management.Core/Models/AdminModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management.Core.Models;
+public class AdminModel
+{
+    public bool AdminStatus { get; set; }
+    public int AdminID { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Designation { get; set; }
+    public DateTime AdminCreationDate { get; set; }
+    public DateTime AdminTerminationDate { get; set; }
+}
