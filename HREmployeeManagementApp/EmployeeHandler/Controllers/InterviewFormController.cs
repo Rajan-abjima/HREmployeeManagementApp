df2f07c654d22da5337547daa943285f644c03cc
Rajan-abjima <125352988+Rajan-abjima@users.noreply.github.com>
2023-07-17 19:14:33 +0530

    17-07-23

---
+++ HREmployeeManagementApp/EmployeeHandler/Controllers/InterviewFormController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeHandler.Controllers;
+public class InterviewFormController : Controller
+{
+	public IActionResult InterviewerForm()
+	{
+		return View();
+	}
+}
